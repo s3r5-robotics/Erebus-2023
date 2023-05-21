fdd9d612efc417e21e8b636a19d33d1b4dd99c62
Rene Potocnik <rene.potocnik1@gmail.com>
2023-05-21 21:17:26 +0200

    Append the relative path to `run.py`
    So you don't have to reload the main file in the webots controller window - just upload `run.py` once, and you're done.

---
+++ code/run.py
@@ -0,0 +1,4 @@
+import sys
+code_path: str = r'C:\Programming\RoboCup_Erebus\Erebus-2023\code'
+sys.path.append(code_path)
+import hello_world  # This script will run when imported, thus starting the program
