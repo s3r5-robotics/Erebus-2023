3906e42c1c29ea7591f3117d0b4b4ac3d27ef3ea
Rene Potocnik <rene.potocnik1@gmail.com>
2023-05-22 09:13:56 +0200

    Add debug flags for debugging

---
+++ code/debug.py
@@ -0,0 +1,11 @@
+# Instead of just writing bare `print` statements, use debug bools which can be toggled from here.
+# Use separate variables for major debugging sections to prevent cluttering the console.
+
+# Debugging example:
+"""
+import debug
+if debugging.POSITION:
+    print(robot.getPosition())
+"""
+
+CAMERA: bool = True
