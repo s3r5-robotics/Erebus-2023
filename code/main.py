3aa0a6d7f00a3e26964d04e9e59a4561b7927cb5
Quantumlyy <ethoarx@gmail.com>
2023-05-31 10:58:48 +0200

    Osnova za kontrolo trenutnega stanja robota

--- code/main.py
+++ code/main.py
@@ -1,21 +1,41 @@
 import debug
+from flow.state_machine import StateMachine
 from robot import Robot
 from utils import Angle
 
 robot = Robot()
+states = StateMachine("init")
+
+
+def init(state):
+    states.change_state("drive")
+
+
+def drive(state):
+    robot.drive.velocity = 1
+
+    if robot.step_counter % 50 == 0:
+        states.change_state("turn")
+
+
+def turn(state):
+    robot.drive.rotation = robot.drive.rotation + Angle(deg=90)
+    states.change_state("drive")
+
+
+states.create_state("init", init, {"drive"})
+states.create_state("drive", drive, {"turn"})
+states.create_state("turn", turn, {"drive"})
 
 while robot():
     if debug.ANY:
         print(f"{robot.step_counter}", end="    ")
 
-    if robot.step_counter == 1:
-        robot.drive.velocity = 1
-    elif robot.step_counter % 100 == 0:
-        robot.drive.rotation = robot.drive.rotation + Angle(deg=90)
-
     if debug.DISTANCE:
         print(f"L|F|R  {robot.distance_l.value:.3f} | {robot.distance_f.value:.3f} | {robot.distance_r.value:.3f}",
               end="    ")
+
+    states.run()
 
     if debug.ANY:
         print(flush=True)
