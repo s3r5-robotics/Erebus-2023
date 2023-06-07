bf4cff083fe0b804f93f005731fa7091d77fd5f8
Rene Potocnik <rene.potocnik1@gmail.com>
2023-06-07 13:39:40 +0200

    remote: add simulation and waldi ip
    + aad `.idea/` to `.gitignore`

--- helper_scripts/remote.bat
+++ helper_scripts/remote.bat
@@ -2,8 +2,10 @@
 
 set PYTHONPATH=%WEBOTS_HOME%\lib\controller\python
 
+:: 192.168.10.18 - Simulation PC IP
+:: 10.0.11.79 - Waldi PC IP
 set WEBOTS_CONTROLLER_URL=tcp://192.168.10.18:1234/Erebus_Bot
 
-python run.py
+python ../code/run.py
 
 pause
