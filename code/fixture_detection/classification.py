afcb97cd39d70073e1443a022eaa8853e1a7ab14
Quantumlyy <ethoarx@gmail.com>
2023-06-10 08:42:21 +0200

    use keras

--- code/fixture_detection/classification.py
+++ code/fixture_detection/classification.py
@@ -1,22 +1,19 @@
+import keras
 import tensorflow as tf
 import cv2 as cv
 
 
 class Classifier:
     MODEL_DATA = {
-        "hsu_rev-1": "models/rgb_hsu_rev-1.lit"
+        "rev-1": "models/rev-1.keras"
     }
 
     def __init__(self, model: str):
-        self.model = self.MODEL_DATA[model]
-
-        self.interpreter = tf.lite.Interpreter(self.model)
-        self.input_details = self.interpreter.get_input_details()
-        self.output_details = self.interpreter.get_output_details()
-
-        self.interpreter.allocate_tensors()
+        self.model = keras.models.load_model(self.MODEL_DATA[model])
 
     def classify_fixture(self, fixture) -> str:
-        image = cv.resize(fixture["image"], (100, 100), interpolation=cv.INTER_AREA)
+        img_array = tf.expand_dims(fixture["image"], 0)
+        predictions = self.model.predict(img_array)
+        predictions_sorted = sorted(map(lambda p, c: (c, p), predictions[0], 3), key=lambda t: t[1])
 
         return ""
