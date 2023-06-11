7d7020754f0be043bbaf151f9f72ac4af4442942
Rene Potocnik <rene.potocnik1@gmail.com>
2023-06-11 17:48:40 +0200

    victim detection: show original and modified images of each camera.

--- VictimDetection/image_plotter.py
+++ VictimDetection/image_plotter.py
@@ -1,7 +1,7 @@
-import math
 from os import PathLike
-from typing import Iterable, Optional
+from typing import Optional, Literal, Union, Sequence, Iterable, Callable
 
+import numpy as np
 from PIL import Image
 
 
@@ -39,25 +39,12 @@ from PIL import Image
 
 
 class ImagePlotter:
-    def __init__(self, width: int, height: int, images_per_row: int, rotate_images: bool = False):
-        self.rotate_images = rotate_images
+    def __init__(self, width: int, height: int, columns: int):
         self.width = width
         self.height = height
-        self.images_per_row = images_per_row
-        self.images: list[Image] = []
+        self.columns = columns
+        self.images: list[list[Image]] = [[] for _ in range(columns)]
         self.image: Optional[Image] = None
-
-    def add_bgra(self, *images: bytes) -> 'ImagePlotter':
-        for img in images:
-            img = Image.frombytes('RGBA', (self.width, self.height), self._bgra_to_rgba(img))
-            self.images.append(img.rotate(90) if self.rotate_images else img)
-        return self
-
-    def add_rgb(self, *images: bytes) -> 'ImagePlotter':
-        for img in images:
-            img = Image.frombytes('RGB', (self.width, self.height), img)
-            self.images.append(img.rotate(90) if self.rotate_images else img)
-        return self
 
     @staticmethod
     def _bgra_to_rgba(bgra: bytes) -> bytearray:
@@ -69,19 +56,62 @@ class ImagePlotter:
             bgra[pxi], bgra[pxi + 2] = bgra[pxi + 2], bgra[pxi]
         return bgra
 
+    def add(self, *images: Union[bytes, Iterable[bytes]], mode: Literal["BRGA", "RGB", "L", "P"],
+            rotations: Union[None, Literal[-90, 0, 90, 180], Sequence[Literal[-90, 0, 90, 180]]] = None,
+            column: int = None) -> list[Image]:
+        """
+        Add images to the row(s) or column of the image grid
+
+        :param images:     The images to add.
+        :param mode:       The mode of the images.
+        :param rotations:  The rotation to apply to all images or separate rotation for each image.
+        :param column:     If None, each image will be added to its own column.
+                           Otherwise, all images will be added to the specified column.
+        """
+        conversion_function, mode = {
+            "BRGA": (self._bgra_to_rgba, "RGBA"),
+        }.get(mode, (lambda x: x, mode))  # type: Callable[[bytes], bytearray], str
+
+        # If images were provided as a list of images (single first argument), use that list
+        if not isinstance(images[0], (bytes, bytearray, np.ndarray)):
+            images = images[0]  # type: Iterable[bytes]
+
+        generated_images = []
+        for i, img in enumerate(images):
+            if rotations is None:
+                rotation = 0
+            elif isinstance(rotations, int):
+                rotation = rotations
+            else:
+                rotation = rotations[i]
+
+            # If rotations is (multiple of) 90 degrees, the width and height are swapped
+            if rotation % 180 == 90:  # Not divisible by 180
+                size = self.height, self.width
+            else:
+                size = self.width, self.height
+
+            img = Image.frombytes(mode, size, conversion_function(img))
+            if rotation:
+                img = img.rotate(rotation)
+
+            if column is None:
+                self.images[i].append(img)
+            else:
+                self.images[column].append(img)
+            generated_images.append(img)
+
+        return generated_images
+
     def generate_image(self, x_margin: int = 1, y_margin: int = 1) -> Image:
-        rows = math.ceil(len(self.images) / self.images_per_row) + 1
+        rows = max(len(column) for column in self.images) + 1
 
-        width = self.height if self.rotate_images else self.width
-        height = self.width if self.rotate_images else self.height
-
-        self.image = Image.new('RGBA', (len(self.images) * (width + y_margin) + y_margin,
-                                        rows * (height + x_margin) + x_margin))  # Create the final image grid
-        for i, im in enumerate(self.images):
-            row = i // self.images_per_row
-            col = i % self.images_per_row
-
-            self.image.paste(im, (y_margin + col * (width + y_margin), x_margin + row * (height + x_margin)))
+        self.image = Image.new('RGBA', (y_margin + self.columns * (self.width + y_margin),
+                                        x_margin + rows * (self.height + x_margin)))  # Create the final image grid
+        for col_i, column in enumerate(self.images):
+            for row_i, image in enumerate(column):
+                self.image.paste(image, (y_margin + col_i * (self.width + y_margin),
+                                         x_margin + row_i * (self.height + x_margin)))
 
         return self.image
 
@@ -89,5 +119,6 @@ class ImagePlotter:
         (image or self.image or self.generate_image()).save(path)
 
     def clear(self) -> None:
-        self.images.clear()
+        for column in self.images:
+            column.clear()
         self.image = None
--- VictimDetection/victim_imager.py
+++ VictimDetection/victim_imager.py
@@ -1,5 +1,6 @@
 import random
 import time
+from pathlib import Path
 
 import cv2 as cv
 import numpy as np
@@ -7,7 +8,6 @@ from PIL import Image
 
 from controller import Robot, Motor, Camera
 from image_plotter import ImagePlotter
-
 
 robot = Robot()
 timestep = int(robot.getBasicTimeStep())
@@ -25,7 +25,7 @@ camera_front.enable(timestep)
 camera_right.enable(timestep)
 camera_left.enable(timestep)
 
-plotter = ImagePlotter(width=64, height=40, images_per_row=3, rotate_images=True)
+plotter = ImagePlotter(width=40, height=64, columns=3)
 
 
 class ColorFilter:
@@ -44,6 +44,12 @@ color_filters = {
     "white": ColorFilter(lower_hsv=(0, 0, 200), upper_hsv=(0, 0, 220)),
     "yellow": ColorFilter(lower_hsv=(25, 140, 82), upper_hsv=(40, 255, 255)),
     "red": ColorFilter(lower_hsv=(160, 150, 127), upper_hsv=(180, 255, 255))
+}
+color_filter_palette = {  # Color for pixel value <=128, color for pixel value >128
+    "black": ([255, 255, 255], [0, 0, 0]),
+    "white": ([0, 0, 0], [255, 255, 255]),
+    "yellow": ([0, 0, 0], [255, 255, 0]),
+    "red": ([0, 0, 0], [255, 0, 0]),
 }
 
 min_fixture_height = 15
@@ -67,30 +73,42 @@ def filter_fixtures(victims) -> list:
     return final_victims
 
 
-def find_fixtures(image) -> tuple[list[Image], list]:
+def find_fixtures(image: Image, camera: int) -> list:
     """
     Finds fixtures in the image.
     Returns a list of dictionaries containing fixture positions and images.
     """
     image = np.rot90(image, k=3)
     binary_images = []
-    for f in color_filters.values():
-        binary_images.append(f.filter(image))
+    for fn, f in color_filters.items():
+        img = f.filter(image)
+        binary_images.append(img)
+
+        # Plot this image in the correct filter color
+        img = plotter.add(img, mode="P", column=camera)[0]
+        pixel0, pixel1 = color_filter_palette[fn]
+        img.putpalette(128 * pixel0 + 128 * pixel1)
 
     binary_image = sum_images(binary_images)
+    rect_image = np.zeros_like(binary_image)
 
     contours, _ = cv.findContours(binary_image, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     for c0 in contours:
         x, y, w, h = cv.boundingRect(c0)
         cv.rectangle(binary_image, (x, y), (x + w, y + h), (225, 255, 255), -1)
+        cv.rectangle(rect_image, (x, y), (x + w, y + h), (128, 128, 128), 1)
     contours, _ = cv.findContours(binary_image, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     final_victims = []
     for c in contours:
         x, y, w, h = cv.boundingRect(c)
+        cv.rectangle(rect_image, (x, y), (x + w, y + h), (255, 255, 255), 1)
         final_victims.append({"image": image[y:y + h, x:x + w], "position": (x, y)})
 
+    plotter.add(binary_image, mode="L", column=camera)
+    plotter.add(rect_image, mode="L", column=camera)
+
     # print("unfiltered", len(final_victims))
-    return binary_images, filter_fixtures(final_victims)
+    return filter_fixtures(final_victims)
 
 
 # def get_unknown_color_clusters(image_path, known_colors_dict):
@@ -140,36 +158,35 @@ def find_fixtures(image) -> tuple[list[Image], list]:
 
 
 images_dir = "C:/Programming/RoboCup_Erebus/FixtureDataset/C"
+debug_images_dir = r"C:\Programming\RoboCup_Erebus\Erebus-2023\VictimDetection\images"
+
+debug_images_dir = Path(debug_images_dir, Path(robot.world_path).stem)
+debug_images_dir.mkdir(parents=True, exist_ok=True)
 
 step_counter = 0
-
 while robot.step(timestep) != -1:
     step_counter += 1
     # Get the images from the cameras
     image_left = camera_left.getImage()
     image_front = camera_front.getImage()
     image_right = camera_right.getImage()
+    images = (image_left, image_front, image_right)
 
     plotter.clear()
-    plotter.add_bgra(image_left, image_front, image_right)
+    plotter.add(images, mode="BRGA", rotations=[90, -90, -90])
 
     # Every {x} time steps, randomly set wheel speeds
     if step_counter % 70 == 0:
         wheel_left.setVelocity(random.uniform(-6.28, 6.28))
         wheel_right.setVelocity(random.uniform(-6.28, 6.28))
 
-    filtered_images: list[list[Image]] = [[], [], []]
-
-    for i, img in enumerate([image_left, image_front, image_right]):
+    for i, img in enumerate(images):
         numpy_image = np.array(np.frombuffer(img, np.uint8).reshape((40, 64, 4)))
-        images, victims = find_fixtures(numpy_image)
-        filtered_images[i] = images
+        victims = find_fixtures(numpy_image, i)
         if len(victims):
-            print(step_counter)
+            print(step_counter, "victim")
             cv.imwrite(f"{images_dir}/{step_counter}-{time.time()}.png", numpy_image)
 
-    for left, front, right in zip(*filtered_images):
-        plotter.add_rgb(left, front, right)
-
-    if step_counter % 50 == 0:
-        plotter.save(rf"C:\Programming\RoboCup_Erebus\Erebus-2023\VictimDetection\images\{step_counter}.png")
+    if step_counter % 10 == 0:
+        plotter.save(debug_images_dir.joinpath(f"{step_counter}.png"))
+        print(step_counter, "image")
